d2b87f783b1761930500a0e36a1afd358e718f7c
Darren Liau <liaukaize@gmail.com>
2023-01-20 16:00:07 +0800

    somewhat completed setting check and input

--- main.cpp
+++ main.cpp
@@ -1,22 +1,104 @@
 // ********************************************************* 
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
-// Lab: TxxL 
-// Names: MEMBER_NAME_1 | MEMBER_NAME_2 | MEMBER_NAME_3 
-// IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 
-// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
-// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// Lab: TT7L 
+// Names: Liau Kai Ze | Lai Cheng Yung
+// IDs: 1211102809 | 1211102810
+// Emails: 1211102809@student.mmu.edu.my | 1211102810@student.mmu.edu.my
+// Phones: 012-5158580 | 010-2528292
 // ********************************************************* 
-
-// TODO: Fill in the missing information above and delete this line.
 
 #include "pf/helper.h"
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Assignment (Part 1)" << endl;
-    cout << "Let's Get Started!" << endl;
-    pf::Pause();
+int zombies = 1;
+
+void resetScreen(){
+
+}
+
+int gameSettings(){
+    int boardX = 10;
+    int boardY = 5;
+    char selection;
+
+    while(true){
+        cout << "Default game settings: " << endl;
+        cout << "Board Rows: " <<  boardX << endl;
+        cout << "Board Columns:  " << boardY << endl;
+        cout << "Number of Zombies: "<< zombies << endl;
+        cout <<"Would You like to change the settings? (y/n)"<<endl;
+        cin >> selection;
+        cout <<endl;
+        
+        if (selection == 'Y' || selection == 'y'){
+
+            cout << "New Board Rows: " ;
+            cin >> boardX;
+            while (true){
+                if(cin.fail()){
+                    cin.clear();
+                    cin.ignore();
+                    cout<<"You have entered wrong input, enter again: "<<endl;
+                    cout << "New Board Rows: " ;
+                    cin>>boardX;
+                }
+                else{
+                    break;
+                }
+            }
+
+            cout << "New Board Columns:  " ;
+            cin >> boardY;
+            while (true){
+                if(cin.fail()){
+                    cin.clear();
+                    cin.ignore();
+                    cout<<"You have entered wrong input, enter again: "<<endl;
+                    cout << "New Board Columns:  " ;
+                    cin>>boardY;
+                }
+                else{
+                    break;
+                }
+            }
+
+            cout << "New Number of Zombies: ";
+            cin >> zombies;
+            while (true){
+                if(cin.fail()){
+                    cin.clear();
+                    cin.ignore();
+                    cout<<"You have entered wrong input, enter again: "<<endl;
+                    cout << "New Number of Zombies: ";
+                    cin>>zombies;
+                }
+                else{
+                    break;
+                }
+            }
+
+            cout << "New settings saved"<< endl;
+        }
+
+        else if (selection == 'N' || selection == 'n'){
+            cout << "no";
+            break;
+        }
+
+        else{
+            cout << "Invalid Selection"<<endl<<endl;
+        }
+    }
+
+    //int board[][];
+    //return board;
+}
+void quitGame(){
+
+}
+
+int main(){
+    gameSettings();
 }
