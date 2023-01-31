1c8cbaaa0e2b32454d1406f258c34b5463481afa
Darren Liau <liaukaize@gmail.com>
2023-01-31 19:01:43 +0800

    added tests and code from materials working map for game is done

--- main.cpp
+++ main.cpp
@@ -10,12 +10,262 @@
 
 #include "pf/helper.h"
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib> // for system()
+#include <ctime>   // for time() in srand( time(NULL) );
+#include <iomanip> // for setw()
 using namespace std;
 
 int zombies = 1;
 
-void resetScreen(){
+class Board
+{
+private:
+    vector<vector<char>> board_; // convention to put trailing underscore
+    int boardX_, boardY_;          // to indicate private data
 
+public:
+    Board(int boardX = 15, int boardY = 5);
+    void init(int boardX, int boardY);
+    void display() const;
+    int getDimX() const;
+    int getDimY() const;
+    char getObject(int x, int y) const;
+    void setObject(int x, int y, int ch);
+    bool isEmpty(int x, int y, char ch);
+    bool isInsideMap(int x, int y, int maxX, int maxY);
+};
+
+bool Board::isEmpty(int x, int y, char ch)
+{
+    return x, y, (isblank(ch));
+}
+
+bool Board::isInsideMap(int x, int y, int maxX, int maxY)
+{
+    return (1<=x &&1<=y) && (x<=maxX && y<=maxY);
+}
+
+char Board::getObject(int x, int y) const
+{
+    return board_[boardY_-y][x-1];
+}
+
+void Board::setObject(int x, int y, int ch)
+{
+    board_[boardY_-y ][x-1 ] = ch;
+}
+
+Board::Board(int boardX, int boardY)
+{
+    init(boardX, boardY);
+}
+
+void Board::init(int boardX, int boardY)
+{
+    boardX_ = boardX;
+    boardY_ = boardY;
+
+    char objects[] = {' ', ' ', ' ', ' ', ' ', ' ', 'X', '#', '@', '$'};
+    int noOfObjects = 10; // number of objects in the objects array
+    // create dynamic 2D array using vector
+
+    board_.resize(boardY_); // create empty rows
+
+    for (int i = 0; i < boardY_; ++i)
+    {
+        board_[i].resize(boardX_); // resize each row
+    }
+
+    // put random characters into the vector array
+    for (int i = 0; i < boardY_; ++i)
+    {
+        for (int j = 0; j < boardX_; ++j)
+        {
+            int objNo = rand() % noOfObjects;
+            board_[i][j] = objects[objNo];
+        }
+    }
+}
+
+void Board::display() const
+{
+    // for each row
+    for (int i = 0; i < boardY_; ++i)
+    {
+        // display upper border of the row
+        cout << " ";
+        for (int j = 0; j < boardX_; ++j)
+        {
+            cout << "+-";
+        }
+        cout << "+" << endl;
+        // display row number
+        cout << setw(2) << (boardY_ - i);
+        // display cell content and border of each column
+
+        for (int j = 0; j < boardX_; ++j)
+        {
+            cout << "|" << board_[i][j];
+        }
+        cout << "|" << endl;
+    }
+
+    // display lower border of the last row
+    cout << " ";
+    for (int j = 0; j < boardX_; ++j)
+    {
+        cout << "+-";
+    }
+    cout << "+" << endl;
+    // display column number
+    cout << " ";
+
+    for (int j = 0; j < boardX_; ++j)
+    {
+        int digit = (j + 1) / 10;
+        cout << " ";
+        if (digit == 0)
+            cout << " ";
+        else
+            cout << digit;
+    }
+
+    cout << endl;
+    cout << " ";
+    for (int j = 0; j < boardX_; ++j)
+    {
+        cout << " " << (j + 1) % 10;
+    }
+    cout << endl
+         << endl;
+}
+
+int Board::getDimX() const
+{
+    return boardX_;
+}
+
+int Board::getDimY() const
+{
+    return boardY_;
+}
+
+void test1_3()
+{
+    Board board;
+cout << "Dim X = " << board.getDimX() << endl;
+cout << "Dim Y = " << board.getDimY() << endl;
+}
+
+void test1_4()
+{
+    Board board;
+    board.display();
+
+    int x, y;
+    char obj;
+
+    x = 1;
+    y = 1;
+    obj = board.getObject(x, y);
+    cout << "Object at column " << x << " and row " << y << " is ["
+        << obj << "]" << endl;
+
+    x = 15;
+    y = 2;
+    obj = board.getObject(x, y);
+    cout << "Object at column " << x << " and row " << y << " is ["
+        << obj << "]" << endl;
+
+    x = 5;
+    y = 5;
+    obj = board.getObject(x, y);
+    cout << "Object at column " << x << " and row " << y << " is ["
+        << obj << "]" << endl;
+
+    x = 10;
+    y = 2;
+    obj = board.getObject(x, y);
+    cout << "Object at column " << x << " and row " << y << " is ["
+        << obj << "]" << endl;
+}
+
+void test1_5()
+{
+    Board board;
+
+    int x1 = 1, y1 = 1;
+    char ch1 = 'A';
+
+    int x2 = 15, y2 = 1;
+    char ch2 = 'B';
+
+    int x3 = 15, y3 = 5;
+    char ch3 = 'C';
+
+    int x4 = 1, y4 = 5;
+    char ch4 = 'D';
+
+    int x5 = 7, y5 = 3;
+    char ch5 = 'E';
+
+    board.setObject(x1, y1, ch1);
+    board.setObject(x2, y2, ch2);
+    board.setObject(x3, y3, ch3);
+    board.setObject(x4, y4, ch4);
+    board.setObject(x5, y5, ch5);
+
+    board.display();
+    cout << "The object " << ch1 << " should be at column "
+         << x1 << " and row " << y1 << endl;
+    cout << "The object " << ch2 << " should be at column "
+         << x2 << " and row " << y2 << endl;
+    cout << "The object " << ch3 << " should be at column "
+         << x3 << " and row " << y3 << endl;
+    cout << "The object " << ch4 << " should be at column "
+         << x4 << " and row " << y4 << endl;
+    cout << "The object " << ch5 << " should be at column "
+         << x5 << " and row " << y5 << endl;
+}
+
+void test1_6()
+{
+    Board board;
+    int maxX = board.getDimX();
+    int maxY = board.getDimY();
+
+    int x1 = 2, y1 = 4;
+    char ch1 = 'Z';
+
+    int x2 = 10, y2 = 3;
+    char ch2 = ' ';
+
+    board.setObject(x1, y1, ch1);
+    board.setObject(x2, y2, ch2);
+    board.display();
+    
+    cout << boolalpha;
+    cout << "Is (" << x1 << ", " << y1 << ") empty? Answer = "
+         << board.isEmpty(x1, y1,ch1) << endl;
+    cout << "Is (" << x2 << ", " << y2 << ") empty? Answer = "
+         << board.isEmpty(x2, y2,ch2) << endl;
+
+    int x3 = 15, y3 = 5;
+    int x4 = 0, y4 = 3;
+    
+    cout << "Is (" << x3 << ", " << y3 << ") inside board? Answer = "
+         << board.isInsideMap(x3, y3,maxX,maxY) << endl;
+    cout << "Is (" << x4 << ", " << y4 << ") inside board? Answer = "
+         << board.isInsideMap(x4, y4,maxX,maxY) << endl;
+
+}
+
+int main(){
+    srand(1); // use this for fixed board during testing
+    // srand(time(NULL)); // this for random board
+    test1_6();
 }
 
 int gameSettings(){
@@ -94,11 +344,4 @@ int gameSettings(){
 
     //int board[][];
     //return board;
-}
-void quitGame(){
-
-}
-
-int main(){
-    gameSettings();
 }
